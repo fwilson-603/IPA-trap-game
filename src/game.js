58bbec15ddf4c25e3fe747d330df5d87f1577e20
fwilson-603 <73718144+fwilson-603@users.noreply.github.com>
2020-11-30 18:15:29 -0500

    Update game.js

--- src/game.js
+++ src/game.js
@@ -11,3 +11,24 @@ function drop(event) {
   var data = event.dataTransfer.getData("text");
   event.target.appendChild(document.getElementById(data));
 }
+
+const manners = ['plosive', 'fricative']
+const places = ['bilabial', 'alveolar', 'palatal', 'velar']
+const rules = {
+  k-symbol: {
+    place: ['velar'],
+    manner: ['plosive']
+},
+  p-symbol: {
+     place: ['bilabial'],
+     manner: ['plosive']
+},
+  c-symbol: {
+     place: ['palatal'],
+     manner: ['plosive']
+},
+  t-symbol: {
+     place: ['alveolar'],
+     manner: ['plosive']
+}
+};
