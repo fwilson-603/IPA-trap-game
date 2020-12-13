9725ec7a04a874936a411d1c72744be356e3795a
fwilson-603 <73718144+fwilson-603@users.noreply.github.com>
2020-12-13 17:04:39 -0500

    Update game.js

--- src/game.js
+++ src/game.js
@@ -41,7 +41,7 @@ function drop0(event) {
 function canCheck(symbol) {
  // var rules = IPArules[symbol];
  // var validPlaces = rules.place;
-  if (validPlaces.includes("velar")) {
+  if (validPlaces.includes(velar)) {
     return true;
   }
   return false;
