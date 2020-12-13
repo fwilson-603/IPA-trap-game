5d1cb5d04a4359943c00f540b66f06c76bedb054
fwilson-603 <73718144+fwilson-603@users.noreply.github.com>
2020-12-13 16:32:55 -0500

    Update game.js

--- src/game.js
+++ src/game.js
@@ -22,7 +22,7 @@ function drop(event) {
   event.preventDefault();
   var data = event.dataTransfer.getData("text");
   event.target.appendChild(document.getElementById(data));
-  if (canCheck(data)) {
+  if (data == "ksymbol") {
     event.target.style.background = '#006400';
   }
   else {
