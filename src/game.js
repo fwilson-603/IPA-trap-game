da48e3016b18f6f73e7243bcd5d6db2251f3f7e9
fwilson-603 <73718144+fwilson-603@users.noreply.github.com>
2020-12-13 17:14:06 -0500

    Update game.js

--- src/game.js
+++ src/game.js
@@ -60,11 +60,6 @@ function testTruth() {
   }
 }
 
-
-function changeText() {
-  document.getElementById('demo').innerHTML='Wow! Some different text!';
-}
-
 var p_symbol = {place:"bilabial", manner:"plosive", voicing:"voiceless", nasality:"oral"};
 var k_symbol = {place:"velar", manner:"plosive", voicing:"voiceless", nasality:"oral"};
 var c_symbol = {place:"palatal", manner:"plosive", voicing:"voiceless", nasality:"oral"};
