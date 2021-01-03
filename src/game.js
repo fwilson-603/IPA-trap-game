f6d569eb02436d7ebba3b2c8380f037722750fb5
fwilson-603 <73718144+fwilson-603@users.noreply.github.com>
2021-01-03 15:16:29 -0500

    Update game.js

--- src/game.js
+++ src/game.js
@@ -105,18 +105,18 @@ function drag(event) {
 function drop(event) {
 	event.preventDefault(); //prevents the default drag-and-drop disallowed
 	var data = event.dataTransfer.getData("text"); //puts the data stored by the drag(event) function (the dragged object's id) in 'data'
-	document.getElementById("test1").innerHTML = typeof data;
+	//document.getElementById("test1").innerHTML = typeof data;
 	var test; //creates the variable 'test'
 	var boxArray = [];  //creates the empty array 'boxArray'
 	var boxId = event.target.id; //creates the variable 'boxId', containing the id of the target (i.e. the box being dropped into)
-	//document.getElementById("test2").innerHTML = boxId;
+	document.getElementById("test1").innerHTML = typeof boxId;
 	var boxAllow = window[boxId]; //creates the variable 'boxAllow', containing the contents of the global variable called by the contents of 'boxId',
 	//this should be an array of the acceptable ids that the box will let pass.
-	//document.getElementById("test3").innerHTML = boxAllow;
+	document.getElementById("test2").innerHTML = typeof boxAllow;
 	event.target.appendChild(document.getElementById(data)); //adds the dragged object to the target (the box)
 	//boxArray.appendChild(boxAllow); //adds the allowed ids for the box to the empty array called 'boxArray'
 	boxArray.push(boxAllow);
-	//document.getElementById("test1").innerHTML = boxArray;
+	document.getElementById("test3").innerHTML = typeof boxArray;
 	test = boxArray.indexOf(data); //if the dragged item's id is in the array of allowed items, 'test' will be set to 0 or higher, if not, 'undefined'
 	//document.getElementById("test2").innerHTML = test;
 	if (test >= 0) { //this should test if the 'test' variable contains the number 0 or higher, and come back green if so, red if not.
