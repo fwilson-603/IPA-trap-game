8b25ae45266bd975720f5dd38bbb78c02b0a90e8
fwilson-603 <73718144+fwilson-603@users.noreply.github.com>
2021-01-05 15:53:02 -0500

    Update game.js

--- src/game.js
+++ src/game.js
@@ -105,19 +105,11 @@ function drag(event) {
 function drop(event) {
 	event.preventDefault(); //prevents the default drag-and-drop disallowed
 	var data = event.dataTransfer.getData("text"); //puts the data stored by the drag(event) function (the dragged object's id) in 'data'
-	//document.getElementById("test1").innerHTML = typeof data;
 	var test; //creates the variable 'test'
-	//var boxArray = [];  //creates the empty array 'boxArray'
 	var boxId = event.target.id; //creates the variable 'boxId', containing the id of the target (i.e. the box being dropped into)
-	//document.getElementById("test1").innerHTML = typeof boxId;
 	var boxAllow = window[boxId]; //creates the variable 'boxAllow', containing the contents of the global variable called by the contents of 'boxId',
 	//this should be an array of the acceptable ids that the box will let pass.
-	document.getElementById("test1").innerHTML = typeof boxAllow;
-	document.getElementById("test2").innerHTML = boxAllow;
 	event.target.appendChild(document.getElementById(data)); //adds the dragged object to the target (the box)
-	//boxArray.appendChild(boxAllow); //adds the allowed ids for the box to the empty array called 'boxArray'
-	//boxArray.push(boxAllow);
-	//document.getElementById("test3").innerHTML = typeof boxArray;
 	test = boxAllow.indexOf(data); //if the dragged item's id is in the array of allowed items, 'test' will be set to 0 or higher, if not, 'undefined'
 	document.getElementById("test3").innerHTML = test;
 	if (test >= 0) { //this should test if the 'test' variable contains the number 0 or higher, and come back green if so, red if not.
@@ -126,23 +118,6 @@ function drop(event) {
 	else {
 		event.target.style.background = '#8B0000';
 	};
-}
-
-function TESTdrop(event) { //remove 'TEST' to run as the main function
-  event.preventDefault();
-  var data = event.dataTransfer.getData("text");
-  event.target.appendChild(document.getElementById(data));
-  document.getElementById("test1").innerHTML = data; //tests what 'data' is by setting the first 'p' in the test zone to 'data'.
-  var rules = IPArules[data]; // Tries to access the value of the id stored in the object 'IPArules' - don't think this'll work
-  document.getElementById("test2").innerHTML = IPArules2; //tests what the 'IPArules2' global variable contains
-  document.getElementById("test3").innerHTML = IPArules2[0]; //tests what index 0 of the 'IPArules2' global variable contains
-  var test = validPlaces.indexOf(data1); //always 0
-  if (test >= 0) {
-    event.target.style.background = '#006400';
-  }
-  else {
-    event.target.style.background = '#8B0000';
-  }
 }
 
 function drop0(event) {
